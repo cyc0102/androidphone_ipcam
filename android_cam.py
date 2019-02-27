5b3326489dbce50d5d1969c85ebadcf2855e18e3
cyc0102 <40230301+cyc0102@users.noreply.github.com>
2019-02-27 11:43:01 +0800

    add comment

--- android_cam.py
+++ android_cam.py
@@ -7,7 +7,7 @@ url= "http://192.168.169.169:8080/shot.jpg"
 while True:
     img_resp = requests.get(url)
     img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
-    img = cv2.imdecode(img_arr,-1)
+    img = cv2.imdecode(img_arr,-1) # -1 CV_LOAD_IMAGE_UNCHANGED
     cv2.imshow('AndroidCam',img)
 
     if cv2.waitKey(1) == 27:   # Press esc to exit
